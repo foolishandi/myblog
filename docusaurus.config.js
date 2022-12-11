70420bc1f8afea6635c653ee2fa0cb92bd4643a9
foolishandi <84659994+foolishandi@users.noreply.github.com>
2022-12-11 17:48:42 +0800

    编辑页链接

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,15 +42,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/foolishandi/myblog/tree/main",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/foolishandi/myblog/tree/main",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
