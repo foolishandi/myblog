312efdf813983b0832a3414a353cdb8b411e23c3
foolishandi <84659994+foolishandi@users.noreply.github.com>
2022-12-31 00:30:17 +0800

    build

---
+++ docs/前端三大件/html/components/useIframe.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef, useState } from "react";
+
+export default function useIframe(ref, selector) {
+  const [size, setSize] = useState({ H: 0, W: 0 });
+  const resRef = useRef(ref ? ref.current : document.querySelector(selector));
+  if (resRef.current) {
+    const _onload = resRef.current.onload;
+    resRef.current.onload = (e) => {
+      if (_onload) _onload();
+      console.log(resRef.current.contentDocument.body.scrollHeight);
+      setSize({
+        H: resRef.current.contentDocument.body.scrollHeight,
+        W: resRef.current.contentDocument.body.scrollWidth,
+      });
+    };
+  }
+  useEffect(() => {
+    resRef.current = ref ? ref.current : document.querySelector(selector);
+  }, [size.H, resRef]);
+  return [
+    size,
+    resRef.current ? resRef.current.contentDocument.body : resRef.current,
+  ];
+}
